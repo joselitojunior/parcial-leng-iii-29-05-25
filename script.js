276df553e260969ee5af0baed7d3f2eb4a71be58
Joselito Júnior <joselitojuniormc@gmail.com>
2025-05-29 17:15:11 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let name, lastName, dni, dateOfBirth, email;
+
+function onLastNameChange(e) {
+    lastName = document.getElementById('lastName').value;
+}
+
+function onNameChange(e) {
+    name = document.getElementById('name').value;
+}
+
+function onDNIChange(e) {
+    dni = document.getElementById('dni').value;
+}
+
+function onDateOfBirthChange(e) {
+    dateOfBirth = document.getElementById('dateOfBirth').value;
+}
+
+function onEmailChange(e) {
+    email = document.getElementById('email').value;
+}
+
+function next() {
+    if (!name || !lastName || !dni || !dateOfBirth || !email) {
+        alert('Por favor, completa todos los campos.');
+        return;
+    }
+    const nationality = prompt('Cuál es tu nacionalidad?');
+    const favoriteColor = prompt('Cuál es tu color favorito?');
+    const petName = prompt('Cuál es el nombre de tu mascote?');
+    const answerElement = document.getElementById("answer");
+    answerElement.innerText = `${nationality}-${favoriteColor}-${petName}`;
+}
